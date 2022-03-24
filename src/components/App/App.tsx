666debc5065b9876caff76d6764a7b0f114e7987
sani2608 <sanikumarsahani@gmail.com>
2022-03-25 02:02:48 +0530

    press start font app tsx

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
+import Pokedex from "../Pokedex/Pokedex";
 
 import './App.css';
 
 class App extends React.Component {
     render () {
         return (
-            <div>
-                <h1>Hello World</h1>
+            <div className="App">
+                <h1>Pokedex</h1>
+                <Pokedex/>
             </div>
         );
     }
